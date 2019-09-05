5ffd2e55eb590d18195d18ce221854366359c6e4
George Zhou <georgezhou89@hotmail.com>
2019-09-04 22:30:26 -0400

    Part 3 Party Invites Video Tutorial

--- PartyInvites-Autumn2019-300613283-QLZ/Controllers/HomeController.cs
+++ PartyInvites-Autumn2019-300613283-QLZ/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PartyInvites_Autumn2019_300613283_QLZ.Models;
 
 namespace PartyInvites_Autumn2019_300613283_QLZ.Controllers
 {
@@ -16,11 +17,18 @@ namespace PartyInvites_Autumn2019_300613283_QLZ.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
         }
 
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            Repository.AddResponse(guestResponse);
+            return View();
+        }
 
     }
 }
