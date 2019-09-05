ecccb618ec61345cea0ea780d9f7acdb040b5e1a
George Zhou <georgezhou89@hotmail.com>
2019-09-04 21:35:08 -0400

    Video Party Invites Tutorial Part 1

---
+++ PartyInvites-Autumn2019-300613283-QLZ/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PartyInvites_Autumn2019_300613283_QLZ.Controllers
+{
+    public class HomeController : Controller
+{
+        //default action method
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("Index");
+        }
+}
+}
